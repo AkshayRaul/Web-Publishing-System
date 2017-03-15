e8bf514556c71011b6c9eaa8ebe39fd09c145644
Kshitij Yerande <kshitijyerande@gmail.com>
2017-03-15 18:42:35 +0530

    Add files via upload
    writer can save file in upload as txt and those files can be read by reader

--- save.php
+++ save.php
@@ -1,6 +1,10 @@
 <?php
 
-$typedtext=$_POST["parameter"];
-echo $typedtext;
-
+$typedtext=$_POST["text"];
+$filename=$_POST["file"];
+chdir("upload");
+$myfile = fopen($filename.".txt", "w") or die("Unable to open file!");
+fwrite($myfile, $typedtext);
+fclose($myfile);
+echo "file saved succesfully";
 ?>
